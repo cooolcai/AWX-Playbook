da05dfe5dcf16717169fc536120d8bc8d6134915
czm@local.lan <915734072@qq.com>
2021-08-13 16:57:14 +0800

    新增节点sql

--- other/SYC-node/py/test.py
+++ other/SYC-node/py/test.py
@@ -5,6 +5,13 @@ import os
 
 var_new_db_c = 'db_commodity_nodex'
 var_new_db_t = 'db_transaction_nodex'
+var_mysql_pwd = 'Ynyc2019a@!'
 
-os.system("mysql -uroot -p'Ynyc2019a@!' -e \"drop database if exists %s;\"" % (line))
-os.system("mysql -uroot -p'Ynyc2019a@!' -e \"create database %s DEFAULT CHARACTER SET utf8;\"" % (line))
+os.system("mysql -uroot -p'%s' -e \"drop database if exists %s;\"" % (var_mysql_pwd,var_new_db_c))
+os.system("mysql -uroot -p'%s' -e \"create database %s DEFAULT CHARACTER SET utf8;\"" % (var_mysql_pwd,var_new_db_c))
+
+os.system("mysql -uroot -p'%s' -e \"drop database if exists %s;\"" % (var_mysql_pwd,var_new_db_t))
+os.system("mysql -uroot -p'%s' -e \"create database %s DEFAULT CHARACTER SET utf8;\"" % (var_mysql_pwd,var_new_db_t))
+
+
+os.system("mysql -uroot -p'I.lw@2020' %s < /mnt/%s.sql" % (line,line))
